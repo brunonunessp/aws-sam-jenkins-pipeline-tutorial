ad26e2924684fc4b98825d5ed3da6833f3308691
brunonunessp <brunonunessp@gmail.com>
2021-04-24 17:27:11 -0300

    voltando

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,12 @@ pipeline {
   agent any
  
   stages {
-  
+    stage('Install sam-cli') {
+      steps {
+        sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
+        stash includes: '**/venv/**/*', name: 'venv'
+      }
+    }
     stage('Build') {
       steps {
         unstash 'venv'
